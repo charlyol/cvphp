cc1a1cb044197425cb8c39879676804fdf78389b
charlyol <147928338+charlyol@users.noreply.github.com>
2024-01-19 11:46:10 +0100

    z

--- form/traitement.php
+++ form/traitement.php
@@ -1,13 +1,40 @@
 <p>Dans le formulaire précédent, vous avez fourni les
     informations suivantes :</p>
 
-<?php
-$action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_SPECIAL_CHARS);
-$renseignement = filter_input(INPUT_POST, 'commentaire', FILTER_SANITIZE_SPECIAL_CHARS);
+<p>Dans le formulaire précédent, vous avez fourni les
+    informations suivantes :</p>
 
-if ($action==1){
-    include 'form.html';
-    header ('Location: resultat.php');
-    exit;
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $civilite = filter_input(INPUT_POST, 'civilite');
+    $nom = filter_input(INPUT_POST, 'nom');
+    $prenom = filter_input(INPUT_POST, 'prenom');
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $raison = filter_input(INPUT_POST, 'raison');
+    $message = filter_input(INPUT_POST, 'message');
+
+    $directoryPath = __DIR__ . '/cv/contact';
+    if (!file_exists($directoryPath)) {
+        mkdir($directoryPath, 0777, true);
+    }
+
+    if (empty($civilite) || empty($nom) || empty($prenom) || empty($email) || empty($raison) || empty($message)) {
+        echo "Erreur : Tous les champs sont obligatoires. Veuillez remplir tous les champs.";
+    } else {
+        if (!$email) {
+            echo "Erreur : L'adresse email n'est pas valide.";
+        } else {
+            $date = new DateTime();
+            $formattedDate = $date->format('Y-m-d-H-i-s');
+            $filePath = $directoryPath . "/cv/contact_$formattedDate.txt";
+
+            $dataToWrite = "Civilité: $civilite\nNom: $nom\nPrénom: $prenom\nEmail: $email\nRaison du contact: $raison\nMessage: $message\n";
+            file_put_contents($filePath, $dataToWrite);
+
+            echo "Les données ont été enregistrées avec succès dans le fichier $filePath";
+        }
+    }
+} else {
+    echo "Erreur : Cette page ne peut être accédée directement.";
 }
 ?>
